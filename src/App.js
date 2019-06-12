ef15a2a3db1be684eb7128bbea12d235df61ec73
kpose <ganihujude@gmail.com>
2019-06-12 03:44:23 +0100

    add avatar and initials

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
     import './App.css';
     import Header from './components/Header';
+    import Avatar from  './components/Avatar';
+
+
     class App extends Component {
       render() {
         return (
+          <div>
           <Header />
+
+          <div className= "Container">
+          <Avatar initials={'FL'} size={35} backgroundColor={'teal'} /> 
+          </div>
+          
+          </div>
         );
       }
     }
