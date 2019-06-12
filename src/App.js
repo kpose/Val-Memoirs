8777fdada8b1ff1ed0cd017da8388f94ed4c4823
kpose <ganihujude@gmail.com>
2019-06-12 23:56:02 +0100

    annd proxy and image component

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import React, { Component } from 'react';
     import './App.css';
     import Header from './components/Header';
 
-   
-
-
     class App extends Component {
       render() {
         return (
           <div>
           <Header />
           </div>
+
+          //<div class="images"></div>
         );
       }
     }
---
+++ src/components/Images.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import InfiniteScroll from 'react=infinite-scroll-component';
+
+export class Images extends Component {
+    state = {
+        
+    }
+    render() {
+        return <div />
+    }
+}
+
+export default Images;
