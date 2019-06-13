815899264f72bcb535a3d8bd3d86c865b5802a52
kpose <ganihujude@gmail.com>
2019-06-13 03:43:06 +0100

    add search bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header';
+import SearchBar from './components/SearchBar/SearchBar';
 
     class App extends Component {
       render() {
         return (
           <div>
           <Header />
+          <div>
+            <SearchBar />
+        </div>
           </div>
         );
       }
